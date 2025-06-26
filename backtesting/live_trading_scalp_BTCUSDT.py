fbe450d07c967491a57598621103ddcee0543a1f
ferreiraluc <l.adrianobf@gmail.com>
2025-06-26 10:57:11 -0300

    update

--- backtesting/live_trading_scalp_BTCUSDT.py
+++ backtesting/live_trading_scalp_BTCUSDT.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('BYBIT_API_SECRET')
 cliente = HTTP(api_key=API_KEY, api_secret=SECRET_KEY)
 
 cripto = 'BTCUSDT'
-tempo_grafico = '15'
+tempo_grafico = '60'
 qtd_velas_stop = 17
 risco_retorno = 5.0
 emas = [9, 21]
