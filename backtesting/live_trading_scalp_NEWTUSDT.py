44930d441c574b11bd40cf6dcce326ffcd4d5f61
ferreiraluc <l.adrianobf@gmail.com>
2025-07-04 16:11:23 -0300

    update live_trading_scalp_NEWTUSDT.py

--- backtesting/live_trading_scalp_NEWTUSDT.py
+++ backtesting/live_trading_scalp_NEWTUSDT.py
@@ -14,10 +14,10 @@ SECRET_KEY = os.getenv('BYBIT_API_SECRET')
 
 cliente = HTTP(api_key=API_KEY, api_secret=SECRET_KEY)
 
-cripto = 'NEWTUSDT'
+cripto = 'PEPEUSDT'
 tempo_grafico = '1'
 qtd_velas_stop = 17
-risco_retorno = 5.0
+risco_retorno = 4.1
 emas = [9, 21]
 ema_rapida = emas[0]
 ema_lenta = emas[1]
